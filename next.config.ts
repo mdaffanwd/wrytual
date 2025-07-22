bd9d2d5dfd46405c83b5e443188409319a98cd0a
mdaffanwd <mohammedaffan2305@gmail.com>
2025-07-22 14:47:11 +0530

    drop_console in next.config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  swcMinifyOptions: {
+    compress: {
+      drop_console: true,
+    },
+  }
 };
 
 export default nextConfig;
